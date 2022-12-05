681f1876d7aa94de34a7c4f62a7ce47d5d24bc57
dec-lied <gavind2559@gmail.com>
2022-12-05 00:04:51 -0500

    final

--- src/cipher.rs
+++ src/cipher.rs
@@ -1,16 +1,36 @@
-// set key as positive to "encode" ana negative to "decode"
-pub fn shift(string: &String, key: i16) -> String
+// shift string by key amount to the right (wrapping at the end of the alphabet)
+pub fn shift(string: &String, key: &u8) -> String
 {
     let mut new_string: String = String::with_capacity(string.len());
     for character in string.chars()
     {
-        new_string.push((character as i16 + (key % 26)) as u8 as char);
+        if character == ' '
+        {
+            new_string.push(' ');
+            continue;
+        }
+
+        if character >= 'a' && character <= 'z'
+        {
+            new_string.push(((((character as u8 + key) - 'a' as u8) % 26) + 'a' as u8) as char);
+        }
+        else if character >= 'A' && character <= 'Z'
+        {
+            new_string.push(((((character as u8 + key) - 'A' as u8) % 26) + 'A' as u8) as char);
+        }
+        else
+        {
+            println!("{} != {}", character as u8, 'a' as u8);
+            panic!("non ascii string entered.");
+        }
     }
 
     return new_string;
 }
 
-pub fn smart_decode(string: &String) -> String
+// given an encoded string, returns the shift key, decoded string, and confidence value in percent
+// if it does not find any valid value, will default to 0 and the default string
+pub fn smart_decode(string: &String) -> (u8, String, f32)
 {
     use std::fs::File;
     use std::io::{self, BufRead};
@@ -22,14 +42,62 @@ pub fn smart_decode(string: &String) -> String
         Err(_) => panic!("couldnt open wordlist in smart_decode")
     };
 
-    let key_map: HashMap::<usize, u32> = HashMap::new();
-
-    for word in string.split(' ')
+    let mut len_without_spaces: usize = string.len();
+    for character in string.chars()
     {
-        for line in io::BufReader::new(wordlist).lines()
+        if character == ' '
         {
-
+            len_without_spaces -= 1;
         }
     }
-    return String::new();
+
+    let mut key_map: HashMap<u8, u32> = HashMap::new();
+
+    for line in io::BufReader::new(&wordlist).lines()
+    {
+        if let Ok(line) = line
+        {
+            for key in 0..26
+            {
+                for word in string.split(' ')
+                {
+                    if self::shift(&word.to_string(), &key).to_ascii_lowercase() == line
+                    {
+                        *(key_map.entry(key).or_insert(0) as &mut u32) += word.len() as u32;
+                    }
+                }
+            }
+        }
+        else
+        {
+            println!("failed to get current line as string")
+        }
+    }
+
+    let mut largest_key: u8 = 0;
+    let mut largest_value: &u32 = &0;
+    for key in 1..26
+    {
+        if let Some(current_entry) = key_map.get_key_value(&key)
+        {
+            if current_entry.1 > largest_value
+            {
+                largest_key = key;
+                largest_value = current_entry.1;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    let largest_string: String = self::shift(&string, &largest_key);
+    let confidence: f32 = (key_map[&largest_key] as f32 / len_without_spaces as f32) * 100.0;
+
+    return (largest_key, largest_string, confidence);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,56 @@
 mod cipher;
+use colored::Colorize;
 
-fn main()
+fn main() -> std::io::Result<()>
 {
-    cipher::smart_decode(&String::from("HI"));
+    let mut input_mode: String = String::new();
+
+    println!("enter mode ({} / {}):", "shift".cyan(), "smart".magenta());
+    std::io::stdin().read_line(&mut input_mode)?;
+
+    if input_mode.trim_end() == "shift"
+    {
+        // string to shift
+        let mut input_string: String = String::new();
+        println!("\nenter a string to {}:", "shift".cyan());
+
+        std::io::stdin().read_line(&mut input_string)?;
+        input_string = String::from(input_string.trim_end());
+
+        // shift key
+        let mut input_key: String = String::new();
+        println!("\nenter a value to {} the string by:", "shift".cyan());
+
+        std::io::stdin().read_line(&mut input_key)?;
+        let input_key: u8 = input_key.trim().parse().expect("failed to parse integer from input key");
+
+        // result
+        println!("\n{}", cipher::shift(&input_string, &input_key).green());
+    }
+    else if input_mode.trim_end() == "smart"
+    {
+        // string to intelligently decode
+        let mut input_string: String = String::new();
+        println!("\nenter a string to {}:", "decode".magenta());
+
+        std::io::stdin().read_line(&mut input_string)?;
+        input_string = String::from(input_string.trim_end());
+
+        // result
+        let (key, string, confidence) = cipher::smart_decode(&input_string);
+        println!
+        (
+            "\n{} decoded with key {} at {}{} confidence",
+            string.green(),
+            key.to_string().red(),
+            confidence.to_string().yellow(),
+            "%".yellow()
+        );
+    }
+    else
+    {
+        println!("\nplease input a valid option ({} / {}):", "shift".cyan(), "smart".magenta());
+    }
+
+    return Ok(());
 }
